9f4e9a7f86a1e8959e686c77ca56cd725549580a
Sundhar-CM <102985446+Sundhar-CM@users.noreply.github.com>
2022-05-18 21:06:37 +0530

    Create Arrayprinting.java

---
+++ Arrayprinting.java
@@ -0,0 +1,10 @@
+public class main {
+  public static void main(String[] args) {
+    int num[]={7,6,5,4,3,2,1};
+    int i;
+    for(i=0;i<num.length;i++)
+    {
+    System.out.println(num[i]);
+    }
+  }
+}
