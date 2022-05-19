7348ea4eac5d7cc33afddcbb956cc106a41ba6c5
Sundhar-CM <102985446+Sundhar-CM@users.noreply.github.com>
2022-05-19 21:30:17 +0530

    Create sum of arrays.java

---
+++ sum of arrays.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    
+    int array[]={1,2,3,4,5,6};
+    int sum=0;
+    for(int number:array)
+    {
+    sum=sum+number;
+    }
+    System.out.println("The sum of array is " + sum);
+  }
+}
