5941721144e8f9760821e360eeea9914e511b7ac
Sundhar-CM <102985446+Sundhar-CM@users.noreply.github.com>
2022-05-22 09:33:20 +0530

    Create Area.java

---
+++ Area.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    int area,a=6,b=8;
+    area=a*b*1/2;
+    System.out.println("The area of triangle is "+area);
+    int l=8,h=9;
+    area=l*h;
+    System.out.println("The area of rectangle is "+area);
+    int length=5;
+    area=length*length;
+    System.out.println("The area of square is "+area);
+  }
+}
