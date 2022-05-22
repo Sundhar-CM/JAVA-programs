62991dc9e621b4aa574f0a43eb58cb8315823965
Sundhar-CM <102985446+Sundhar-CM@users.noreply.github.com>
2022-05-22 09:24:33 +0530

    Create Swapping.java

---
+++ Swapping.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    int a=95,b=85,temp;
+    System.out.println("Before Swapping a= "+a);
+    System.out.println("After Swapping b= "+b);
+    temp=a;
+    a=b;
+    b=temp;
+    System.out.println("Before Swapping a= "+a);
+    System.out.println("After Swapping b= "+b);
+  }
+}
